5a7e2fc7a2e31e8d47a5140eab805c89d75af839
oyinbunughan okpalo <adrielokpalo@gmail.com>
2026-08-02 11:31:48 +0100

    created reusable header

--- js/ui.js
+++ js/ui.js
@@ -1,19 +1,46 @@
-const hamburger = document.querySelector('.hamburger')
-const overlay = document.querySelector('.overlay')
-const hamMenu = document.querySelector('.hamburger-menu')
-const body = document.body;
 
-hamburger.addEventListener('click', () => {
+
+const showHeader = () => {
+
+    const hamburger = document.querySelector('.hamburger')
+    const overlay = document.querySelector('.overlay')
+    const hamMenu = document.querySelector('.hamburger-menu')
+    const body = document.body;
+
+    console.log(overlay)
+    console.log(hamMenu)
+    console.log(hamburger)
+
+    hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active')
     hamMenu.classList.toggle('active')
     overlay.classList.toggle('active')
     body.classList.toggle('active')
 
-})
+    })
 
-overlay.addEventListener('click', () => {
-    hamburger.classList.remove('active')
-    hamMenu.classList.remove('active')
-    overlay.classList.remove('active')
-    body.classList.remove('active')
-})
+    overlay.addEventListener('click', () => {
+        hamburger.classList.remove('active')
+        hamMenu.classList.remove('active')
+        overlay.classList.remove('active')
+        body.classList.remove('active')
+    })
+
+
+}
+
+async function getHeader() {
+    
+    const result = await fetch('header.html')
+    const data = await result.text()
+
+    const headerContainer = document.querySelector('.header-container')
+
+    headerContainer.innerHTML  = data;
+
+    showHeader()
+
+    
+}
+
+getHeader()
